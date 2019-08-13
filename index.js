517bb10f9be96896660009f36fb4b3db553135e8
Jo Espina <joananespina@gmail.com>
2019-08-13 13:28:12 +0800

    Update integer validity check with Number.isInteger
    Replaced our StackOverflow solution (https://stackoverflow.com/questions/14636536/how-to-check-if-a-variable-is-an-integer-in-javascript)
    
    with a vanilla javascript solution (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/isInteger#Polyfill)

--- index.js
+++ index.js
@@ -1,31 +1,52 @@
-const generator = ({characterSet, length}) => {
+// Randominator
+
+// isInteger Polyfill
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/isInteger#Polyfill
+const isInteger = Number.isInteger || function (value) {
+  return typeof value === 'number' &&
+    isFinite(value) &&
+    Math.floor(value) === value;
+};
+
+const tokenGenerator = ({ characterSet, length }) => {
+  // tokenGenerator generates a token from a given (characterSet) with a specific (length)
+  // this is an internal method
+
   // https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
   // High performance method to generate random string (Not for generating secure tokens)
 
-  return new Promise((resolve, reject)=>{
+  return new Promise((resolve, reject) => {
     let charsToUse = '';
 
     if (Array.isArray(characterSet) === true) {
-      if (characterSet.includes('lowercase') === false
-      && characterSet.includes('uppercase') === false
-      && characterSet.includes('number') === false) {
+      // characterSet is an array
+      if (characterSet.includes('lowercase') === false &&
+      characterSet.includes('uppercase') === false &&
+      characterSet.includes('number') === false) {
+        // Invalid values in characterSet Array
         return reject(new Error('invalid_character_set_array'));
       }
 
       if (characterSet.includes('lowercase') === true) {
+        // Add lowercase letters to charactersToUse for Token
         charsToUse += 'abcdefghijklmnopqrstuvwxyz';
       }
 
       if (characterSet.includes('uppercase') === true) {
+        // Add uppercase letters to charactersToUse for Token
         charsToUse += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
       }
 
       if (characterSet.includes('number') === true) {
+        // Add numbers to charactersToUse for Token
         charsToUse += '0123456789';
       }
     } else if (typeof characterSet === 'string') {
+      // characterSet is a string
+      // User specifically wants to use this set of characters to use for the Token
       charsToUse = characterSet;
     } else {
+      // Invalid characterSet value - unknown characterSet
       return reject(new Error('unknown_character_set'));
     }
 
@@ -33,7 +54,7 @@ const generator = ({characterSet, length}) => {
     const charactersLength = characters.length;
     let result = '';
 
-    for ( let i = 0; i < length; i++ ) {
+    for (let i = 0; i < length; i++) {
       result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
 
@@ -42,51 +63,65 @@ const generator = ({characterSet, length}) => {
 };
 
 const intGenerator = (min, max) => {
+  // Randomly generate an integer from min to max
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random#Getting_a_random_integer_between_two_values_inclusive
   min = Math.ceil(min);
   max = Math.floor(max);
 
-  // The maximum is inclusive and the minimum is inclusive
+  // Both maximum and minimum is inclusive
   const result = Math.floor(Math.random() * (max - min + 1)) + min;
 
   return result;
 };
 
-const generateInt = (min, max) => {
-  // https://stackoverflow.com/questions/14636536/how-to-check-if-a-variable-is-an-integer-in-javascript
+function generateInt (min, max) {
+  return new Promise((resolve, reject) => {
+    let result = null;
 
-  return new Promise((resolve, reject)=>{
-    if (min === undefined) {
-      return reject(new Error('missing_min_value'));
+    try {
+      // Min and max are required
+      if (min === undefined) {
+        return reject(new Error('missing_min_value'));
+      }
+
+      if (max === undefined) {
+        return reject(new Error('missing_max_value'));
+      }
+
+      // Test if min and max values are integers
+
+      const isMinInteger = isInteger(min);
+      const isMaxInteger = isInteger(max);
+
+      if (isMinInteger === false) {
+        throw new Error('non_integer_min_value');
+      }
+
+      if (isMaxInteger === false) {
+        throw new Error('non_integer_max_value');
+      }
+
+      // Min value must not exceed max value
+      if (min > max) {
+        return reject(new Error('min_value_greater_than_max_value'));
+      }
+      
+      result = intGenerator(min, max);
+
+    } catch (error) {
+      return reject(error);
     }
-
-    if (min !== parseInt(min, 10)) {
-      return reject(new Error('non_integer_min_value'));
-    }
-
-    if (max === undefined) {
-      return reject(new Error('missing_max_value'));
-    }
-
-    if (max !== parseInt(max, 10)) {
-      return reject(new Error('non_integer_max_value'));
-    }
-
-    if (min > max) {
-      return reject(new Error('min_value_greater_than_max_value'));
-    }
-
-    const result = intGenerator(min, max);
 
     return resolve(result);
+
   });
 };
 
-const generateToken = (options) => {
-  // options.characterSet = (ARR or STR) ['lowercase', 'uppercase', 'number'] or 'ABCdef123!@#';
-  // options.length = (INT) any number / performance hit for long length
+function generateToken (options) {
+  // options.characterSet = (Array of String or String) ['lowercase', 'uppercase', 'number'] or 'ABCdef123!@#';
+  // options.length = (Integer) any number / performance hit for long length
 
-  return new Promise((resolve)=>{
+  return new Promise((resolve, reject) => {
     if (options === undefined) {
       options = {};
     }
@@ -99,9 +134,9 @@ const generateToken = (options) => {
       options.length = 8;
     }
 
-    generator(options).then((result)=>{
+    tokenGenerator(options).then((result) => {
       return resolve(result);
-    }).catch((error)=>{
+    }).catch((error) => {
       if (error.message === 'unknown_character_set') {
         error = new Error('generateToken({characterSet}) must be String or Array.');
       } else if (error.message === 'invalid_character_set_array') {
